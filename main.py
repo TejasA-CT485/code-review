839a1e9a63ead5e1055c3e6b2498c4e415d08dbb
Tejas Aghade <tejas.aghade@cctech.co.in>
2025-05-06 14:19:18 +0530

    test

--- main.py
+++ main.py
@@ -1,10 +1,1 @@
-import os
-import sys
-import re  # ❌ unused import
 
-import json  # ❌ import order issue
-
-def greet(name):
-    print(  f"Hello, {name}"  )  # ❌ bad formatting (spacing)
-
-greet("world")
